4b3df4bbca1e81c090a5626bc8ecbb616a61b412
Unknows <firstpan555@gmail.com>
2020-02-19 02:01:26 +0700

    [UPDATE]Rename Experience to Social

---
+++ src/components/Social/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Box from '../Core/Box'
+
+export default class Index extends Component {
+  render() {
+    return (
+        <Box>
+         
+        </Box>
+    )
+  }
+}
