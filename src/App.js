7e67e8886e2b95df2d1ae89e353c0fa39b86acdd
Unknows <firstpan555@gmail.com>
2020-02-19 02:03:00 +0700

    [UPDATE]ADD parallaxprovider and overflow-x hiiden in sec/App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react'
+import styled from 'styled-components'
+import { ParallaxProvider } from 'react-scroll-parallax';
 
 import '../node_modules/bootstrap/dist/css/bootstrap.css?v=1'
 
 import Index from './components/Core/index'
 
+const OverFlow = styled.div`
+  overflow-x:hidden;
+`
+
 const App = () => {
   return (
-    <Index />
+    <OverFlow>
+      <ParallaxProvider>
+        <Index />
+      </ParallaxProvider>
+    </OverFlow>
   )
 }
 
