6c99654dbe797f2cc43c294fb44902f31872ce7c
Unknows <firstpan555@gmail.com>
2020-02-19 02:04:14 +0700

    [UPDATE]Responsive in Porfile

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -2,11 +2,16 @@ import React, { Component } from 'react'
 import styled from 'styled-components'
 import dayjs from 'dayjs'
 
+import Box from '../Core/Box.js'
+
 // const datenow = dayjs.format('DD-MM-YYY')
 // const birthday = dayjs('25-11-1999')
 
+
+
 const Img = styled.img`
   border-radius:50%;
+  height:auto;
 `
 
 const Link = styled.a`
@@ -17,23 +22,28 @@ const Link = styled.a`
   }
 `
 
+const Div = styled.div`
+  width:auto;
+`
+
 export default class Index extends Component {
   render() {
     return (
-      <div className="container mt-5">
-        <div className="row justify-content-around">
-          <Img src="/images/profile.jpg" className="col-4" />
-          <div className="col-6 d-flex justify-content-start align-items-center ml-5">
-            <div className="flex-column ">
+      <Box className="justify-content-center mt-5 mt-md-0">
+        <div className="d-flex flex-column flex-md-row justify-content-center">
+          <Img src="/images/profile.jpg" className="w-75 col-md-4 mr-5 ml-5 ml-md-0 pr-md-5" />
+          <Div className="col-md-5 text-center text-md-left d-flex align-items-md-center ml-md-5 pl-md-5 pl-1 pr-1 mt-3 mt-md-0">
+            <div className="flex-column">
               Name : Natthasit Panjapornpol <br />
               Age: 20 years <br />
               Email: <Link href="mailto:firstpan555@gmail.com">firstpan555@gmail.com</Link> <br />
               Telephone Number: <Link href="tel://0969622249">096962249</Link> <br />
-              Graduate: Bachelor degree, School of Information Technology <br /> <span className="pl-5">at King Mongkut's University of Technology Thonburi</span>
+              <div class="pr-0 pr-md-5 pl-0 pl-md-0">Graduate: Bachelor degree of Information Technology, School of Information Technology at KMUTT</div>
             </div>
-          </div>
+
+          </Div>
         </div>
-      </div>
+      </Box>
     )
   }
 }
