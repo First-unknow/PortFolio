ad0b9862a2ff2e932b612aecc5b2759079414f8c
Unknows <firstpan555@gmail.com>
2020-02-22 10:44:26 +0700

    [UPDATE]Profile UI

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -15,7 +15,8 @@ const Img = styled.img`
 `
 
 const Link = styled.a`
-  color:black;
+  /* color:black; */
+  color:#FFFFFF;
   &:hover{
     color:gray;
     text-decoration:none;
@@ -23,26 +24,73 @@ const Link = styled.a`
 `
 
 const Div = styled.div`
+  color:#FFFFFF;
   width:auto;
+  height:50vh;
+  font-size: 20px;
+  @media (max-width:768px) {
+    font-size: 16px;
+  }
+`
+
+const Qoute = styled.q`
+  font-size: 40px;
+  @media (max-width:768px) {
+    font-size: 30px;
+  }
+  @media (max-width:360px) {
+    font-size: 25px;
+  }
+`
+
+const QouteDiv = styled.div`
+  height:50vh;
+`
+
+const BlackDrop = styled.div`
+  margin-top:4em;
+  backdrop-filter: grayscale(0.5) opacity(0.7) ;
+  @media (max-width:768px){
+     backdrop-filter: grayscale(0.5) opacity(0.5) ;
+  }
 `
 
 export default class Index extends Component {
   render() {
     return (
-      <Box className="justify-content-center mt-5 mt-md-0">
-        <div className="d-flex flex-column flex-md-row justify-content-center">
-          <Img src="/images/profile.jpg" className="w-75 col-md-4 mr-5 ml-5 ml-md-0 pr-md-5" />
-          <Div className="col-md-5 text-center text-md-left d-flex align-items-md-center ml-md-5 pl-md-5 pl-1 pr-1 mt-3 mt-md-0">
-            <div className="flex-column">
+      // <Box className="d-flex flex-column justify-content-center mt-3 mt-md-0" >
+      //   <div className="d-flex flex-column flex-lg-row justify-content-center justify-content-lg-around mt-5 mt-lg-0 mb-0 mb-lg-5 ml-0 ml-lg-5">
+      //     <div className="d-flex justify-content-center col-6 col-lg-3">
+      //       <Img src="/images/profile.jpg" />
+      //     </div>
+      //     <Qoute className="d-flex justify-content-center col-12 col-lg-9 align-items-center mt-2 mt-lg-0"><b className="text-center">If fear to fail. Your skill doesn’t improve</b></Qoute>
+      //   </div>
+      //   <Div className="d-flex justify-content-center pl-3 pl-lg-0 pr-3 pr-lg-0">
+      //     <div className="flex-column">
+      //       Name : Natthasit Panjapornpol <br />
+      //       Age: 20 years <br />
+      //       Email: <Link href="mailto:firstpan555@gmail.com">firstpan555@gmail.com</Link> <br />
+      //       Telephone Number: <Link href="tel://0969622249">096962249</Link> <br />
+      //       <div className="pr-0 pr-md-5 pl-0 pl-md-0">Graduate: Bachelor degree of Information Technology, School of Information Technology at KMUTT</div>
+      //     </div> 
+      //     </Div>
+      // </Box>
+      <Box background="url(/images/profile.jpg); background-repeat:no-repeat; background-size:cover; background-position:center;">
+        <div className="row">
+          <QouteDiv className="justify-content-center text-center justify-content-lg-end align-items-center align-items-lg-end row w-100">
+            <Qoute className="col-12 col-lg-4 mt-2 mt-lg-0 pr-0"><b className="text-center pr-5">If fear to fail.Your skill doesn’t improve</b></Qoute>
+          </QouteDiv>
+          <Div className="col-12 text-center d-flex justify-content-center align-items-center">
+            <BlackDrop className="flex-column p-2">
               Name : Natthasit Panjapornpol <br />
               Age: 20 years <br />
               Email: <Link href="mailto:firstpan555@gmail.com">firstpan555@gmail.com</Link> <br />
               Telephone Number: <Link href="tel://0969622249">096962249</Link> <br />
-              <div class="pr-0 pr-md-5 pl-0 pl-md-0">Graduate: Bachelor degree of Information Technology, School of Information Technology at KMUTT</div>
-            </div>
-
+              <div className="pr-0 pr-md-5 pl-0 pl-md-0">Graduate: Bachelor degree of Information Technology, School of Information Technology at KMUTT</div>
+            </BlackDrop>
           </Div>
         </div>
+
       </Box>
     )
   }
