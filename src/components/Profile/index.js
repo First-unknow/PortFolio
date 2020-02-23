c19fd9080f6de169e2ea044117b25f9ec906fd6a
Unknows <firstpan555@gmail.com>
2020-02-23 11:19:19 +0700

    [UPDATE]Add Button Resume and Transcript

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,11 +1,8 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
-// import dayjs from 'dayjs'
 
 import Box from '../Core/Box.js'
-
-// const datenow = dayjs.format('DD-MM-YYY')
-// const birthday = dayjs('25-11-1999')
+import Button from './Button'
 
 const Div = styled.div`
   color:#000000;
@@ -43,6 +40,7 @@ const BlackDrop = styled.div`
 `
 
 export default class Index extends Component {
+
   render() {
     return (
       <Box background="url(/images/profile.jpg); background-repeat:no-repeat; background-size:cover; background-position:center;">
@@ -50,11 +48,15 @@ export default class Index extends Component {
           <div className="justify-content-center text-center justify-content-lg-between align-items-lg-center row w-100">
             <Qoute className="col-12 col-lg-4 mt-2 mt-lg-0 ml-3 mr-3 px-5 pr-lg-5 d-flex align-items-center"><b className="text-center pr-2 pl-2">" If fear to fail,  your skill doesn’t improve "</b></Qoute>
             <Div className="col-12 col-lg-4 p-0 d-flex align-items-end ">
-              <BlackDrop className="flex-column text-center px-5 p-3 p-lg-5 d-flex justify-content-center align-items-lg-center">
+              <BlackDrop className="flex-column text-center ml-2 px-5 py-3 p-lg-5 d-flex justify-content-center align-items-lg-center">
                 Name: Natthasit Panjapornpol <br />
-                Age: 20 years <br />
+                Age: 20 years <br />  
                 Birthday: 25 November 1999 <br />
-                <div className="pr-0 pr-md-5 pl-0 pl-md-0">Cerrently study in Bachelor degree of Information Technology year 2<sup>nd</sup> , School of Information Technology at KMUTT</div>
+                <div className="pr-0 pl-0">Currently study in Bachelor degree of Information Technology year 2<sup>nd</sup> , School of Information Technology at KMUTT</div>
+                <div className="w-100 d-flex justify-content-around mt-2">
+                  <Button href="https://drive.google.com/open?id=1BLeI-b5S69_URyTh7Dpk9k0j-FdzsFEq" hover="Go" show="Resume"/>
+                  <Button href="#" hover="Go" show="TranScript"/>
+                </div>
               </BlackDrop>
             </Div>
           </div>
