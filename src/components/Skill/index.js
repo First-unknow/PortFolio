cd586c065dfa98f59c1400c9a204ea8869b8e7c6
First-unknow <firstpan555@gmail.com>
2020-10-26 16:53:24 +0700

    [UPDATE] fixed bug and change layout

--- src/components/Skill/index.js
+++ src/components/Skill/index.js
@@ -13,14 +13,15 @@ const ReBox = styled(Box)`
 `
 
 const Img = styled.img`
-  width:auto;
+  width:${props => props.size ? props.size : '64px'};
   height:auto;
 `
 
 const LogoSkill = (props) => (
-  <div className={`${props.className} col-12 mt-3`}>
-    <Img src={props.src} className="mt-1 col-6 text-right" />
-    <span className="col-6">{props.text}</span><br />
+  <div className={`${props.className} col-12 mt-3 text-md-left text-center`}>
+    <Img src={props.src} className="mt-1 col-6 text-right" size={props.size} />
+    {console.log()}
+    <span className={`${props.text[0] === 'J' ? 'pl-2' : ''} col-6`}>{props.text}</span><br />
   </div>
 )
 
@@ -45,17 +46,17 @@ export default class Index extends Component {
   render() {
     return (
       <ReBox background="#164563" className="row justify-content-md-between justify-content-center align-items-center w-100 m-0 pb-5 pt-3">
-        <div class="col-12 col-md-6 text-center p-2 mt-5 pt-5 mt-lg-0">
-          <h2>My Skill</h2>
-          <div className="row">
-            <div class="col-12 col-md-6">
-              <h4 className="text-center pl-4 mt-3">Front-End</h4>
+        <div class="col-12 col-md-8 text-center mb-0 mb-md-5 mt-lg-0">
+          <h2 class="mr-md-5 pr-md-5">My Skill</h2>
+          <div className="row justify-content-center">
+            <div class="col-12 col-md-3 mb-3">
+              <h4 className="text-center text-md-left pl-md-4 mt-3">Front-End</h4>
               <div className="row">
                 {
                   this.state.FE.map((data, i) => (
                     <LogoSkill
                       key={i}
-                      className="pl-4 text-center"
+                      className="pl-4"
                       src={`/images/skill/${data.img}.png`}
                       text={data.text}
                     />
@@ -64,53 +65,58 @@ export default class Index extends Component {
               </div>
             </div>
 
-            <div class="col-12 col-md-6">
-              <h4 className="text-center pl-4 mt-3">Back-End</h4>
-                <div className="row">
-                  {
-                    this.state.BE.map((data, i) => (
-                      <LogoSkill
-                        key={i}
-                        className="pl-2 text-center"
-                        src={`/images/skill/${data.img}.png`}
-                        text={data.text}
-                      />
-                    ))
-                  }
-                </div>
-              <h4 className="text-center pl-4 mt-5">Mobile Application</h4>
-                <div className="row">
-                      <LogoSkill
-                        className="pl-2 text-center"
-                        src={`/images/skill/${this.state.MA.img}.png`}
-                        text={this.state.MA.text}
-                      />
-                </div>
+            <div class="col-12 col-md-3 pl-md-4 mb-3">
+              <h4 className="text-center text-md-left pl-md-4 mt-3">Back-End</h4>
+              <div className="row ml-md-0 pl-md-0">
+                {
+                  this.state.BE.map((data, i) => (
+                    <LogoSkill
+                      key={i}
+                      className={`${data.img === 'python' ? 'pl-3' : 'pl-2'} `}
+                      src={`/images/skill/${data.img}.png`}
+                      text={data.text}
+                      size={data.img === 'java' ? '80px' : '64px'}
+                    />
+                  ))
+                }
+              </div>
             </div>
-          </div>
+            <div class="col-12 col-md-3 p-md-0">
+              <h4 className="text-center text-md-left pl-2 pl-md-4 mt-3">Mobile Application</h4>
+              <div className="row pl-md-0 ml-md-1">
+                <LogoSkill
+                  className="pl-2"
+                  src={`/images/skill/${this.state.MA.img}.png`}
+                  text={this.state.MA.text}
+                  size="64px"
+                />
+              </div>
+            </div>
+            </div>
+
         </div>
 
         <Div className="flex-column col-12 col-md-4 text-center mt-5 pb-3 pb-md-0">
           <h2>My Experience</h2>
           <div className="text-md-left mt-4">
             <h4>2018</h4>
-              <h5 className="pl-2">React</h5>
-                <div className="pl-4">- Web PR for WIPCamp 11</div>
+            <h5 className="pl-2">React</h5>
+            <div className="pl-4">- Web PR for WIPCamp 11</div>
             <h4>2019</h4>
-              <h5 className="pl-2">React</h5>
-                <div className="pl-4">- Web PR for Helloworld Cockatiel</div>
-                <div className="pl-4">- Web Register for WIPCamp 12</div>
-              <h5 className="pl-2">Project Manager</h5>
-                <div className="pl-4">- Web PR for WIPCamp 12</div>
-                <div className="pl-4">- Web PR for Helloworld Cockatiel</div>
+            <h5 className="pl-2">React</h5>
+            <div className="pl-4">- Web PR for Helloworld Cockatiel</div>
+            <div className="pl-4">- Web Register for WIPCamp 12</div>
+            <h5 className="pl-2">Project Manager</h5>
+            <div className="pl-4">- Web PR for WIPCamp 12</div>
+            <div className="pl-4">- Web PR for Helloworld Cockatiel</div>
 
             <h4>2020</h4>
-              <h5 className="pl-2">React</h5>
-                <div className="pl-4">- Web Management for WIPCamp 12</div>
-              <h5 className="pl-2">Vue</h5>
-                <div className="pl-4">- Front End Developer Internship with Backyard Co., Ltd</div>
-                <div className="pl-4">- Freelance</div>
-            </div>
+            <h5 className="pl-2">React</h5>
+            <div className="pl-4">- Web Management for WIPCamp 12</div>
+            <h5 className="pl-2">Vue</h5>
+            <div className="pl-4">- Front End Developer Internship with Backyard Co., Ltd</div>
+            <div className="pl-4">- Freelance</div>
+          </div>
         </Div>
       </ReBox>
     )
